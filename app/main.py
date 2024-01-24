e3a7556563125c9de2ca13543e961c5e2c6e30c9
tabee <bee.mario@gmail.com>
2024-01-24 12:52:44 +0000

    add a HTMLParser to to extract from the urls.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,28 @@
 from sitemap_parser import SitemapParser
+from html_parser import HTMLParser
 
+# get a list of all URLs in the sitemap we want to crawl
 parser = SitemapParser('https://www.eak.admin.ch/eak/de/home.sitemap.xml')
 
 try:
-    filtered_urls = parser.get_urls(filter_str="/de/home/Firmen/")
-    print(filtered_urls)
+    filtered_urls = parser.get_urls(filter_str="/eak/de/home/Firmen/familienzulagen/vorgehen/")
+    print(f"\nWe found {len(filtered_urls)} URLs matching the filter.")
+    #print(filtered_urls)
 except Exception as e:
     print(f"An error occurred: {e}")
+
+
+# extract information from each page
+parser = HTMLParser()
+
+for url in filtered_urls:
+    print(f"\nExtracting information from page:\n{url}")
+    try:
+        page_title = parser.get_title(url)
+        print(f"Page title:\n{page_title}")
+
+        page_lead = parser.get_content_by_class(url, 'lead')
+        print(f"Page lead:\n{page_lead}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
